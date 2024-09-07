852b3e75f758e4bc4a99bc9c1592c7bc0af2a46a
William Gajeski <wsgajesk@colossus.it.mtu.edu>
2024-09-06 21:45:49 -0400

    hw 3

---
+++ w2hw1/hw1_q3.c
@@ -0,0 +1,26 @@
+/*
+ * Exponential Form HW
+ *
+ * @name: William Gajeski
+ * @email: wsgajesk@mtu.edu
+ * @date: 9/6/2024
+ */
+
+
+#include <stdio.h>
+
+int main() {
+    double input;
+
+    // Input
+    printf("Please enter a real number: ");
+
+    // Number from person
+
+    scanf("%lf", &input);
+
+    // Prints with Exponent Form
+    printf("Exponent form: %.2e\n", input);
+
+    return 0;
+}
